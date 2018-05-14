bd7de16b5866d65d22d0e28819042ec75daad13a
Zeke Sikelianos <zeke@sikelianos.com>
2018-05-14 08:49:25 -0700

    encodeURIComponent

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ async function lookup (query, locale = 'en') {
   const url = URL.format({
     protocol: 'https',
     hostname: `${locale}.wikipedia.org`,
-    pathname: `/api/rest_v1/page/summary/${query}`
+    pathname: `/api/rest_v1/page/summary/${encodeURIComponent(query)}`
   })
-  console.log(url)
 
   let body
   try {
     const res = await get(url, {json: true})
     body = res.body
   } catch (err) {
-    console.error(err)
     return null
   }
 
